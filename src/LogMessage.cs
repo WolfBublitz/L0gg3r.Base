ee973a8e767db74f32d9aef2d04844b213df0ed9
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-07-31 20:02:54 +0200

    added Payload and Senders attribute to LogMessage

--- src/LogMessage.cs
+++ src/LogMessage.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace L0gg3r.Base;
 
 /// <summary>
@@ -5,5 +7,31 @@ namespace L0gg3r.Base;
 /// </summary>
 public readonly struct LogMessage
 {
+    private readonly List<string> senders;
+
+    public LogMessage(LogLevel logLevel, object payload, string sender)
+    {
+        LogLevel = logLevel;
+        Payload = payload;
+
+        senders = new List<string> { sender };
+    }
+
+    /// <summary>
+    /// Gets the <see cref="LogLevel"/>.
+    /// </summary>
+    /// <seealso cref="LogLevel"/>
     public LogLevel LogLevel { get; }
+
+    /// <summary>
+    /// Gets the payload.
+    /// </summary>
+    public object Payload { get; }
+
+    /// <summary>
+    /// Gets the list of sender.
+    /// </summary>
+    public IEnumerable<string> Senders => senders;
+
+    internal void AddSender(string sender) => senders.Add(sender);
 }
